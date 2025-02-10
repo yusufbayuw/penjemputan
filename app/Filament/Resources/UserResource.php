98a4e592c0147af0d9914e22a4bab93af243aee5
Yusuf Bayu Wicaksono <yusuf.wicaksono@tarunabakti.or.id>
2025-02-10 07:38:53 +0700

    user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -33,8 +33,10 @@ class UserResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
+                    ->unique(ignoreRecord:true)
                     ->maxLength(255),
                 Forms\Components\TextInput::make('username')
+                    ->unique(ignoreRecord:true)
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
